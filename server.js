2c43325e969b60748f47dd0a0c5036388f6d46e0
raza2000 <raza@atliq.com>
2022-01-18 12:07:28 +0530

    some changes

--- server.js
+++ server.js
@@ -4,6 +4,41 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
+// const cors = require("cors");
+// // const express = require("express");
+// require("dotenv").config();
+// // const bodyParser = require("body-parser");
+// // require("./auth/passport");
+// // const app = express();
+
+// // var corsOptions = {
+// //   origin: "http://localhost:8081",
+// // };
+
+// //middleware
+// app.use(cors()); //corsOptions
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// //routers
+// const router = require("./routes/movieRouter.js");
+// app.use("/api", router);
+
+// //testting api
+// app.get("/", (req, res) => {
+//   res.json({ message: "No Issue Found" });
+// });
+
+// //port
+// const PORT = process.env.PORT || 8080;
+
+// //server
+// app.listen(PORT, () => {
+//   console.log(`server is running on port ${PORT}`);
+// });
+
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
@@ -26,4 +61,6 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+
+
+server.listen(8080)
